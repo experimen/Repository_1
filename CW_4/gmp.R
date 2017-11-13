94d3d215c8eabf1ccba52d5509c468fd42febcac
Zubareva Marina <32264363+zubarevnka@users.noreply.github.com>
2017-11-13 23:02:22 +0500

    Update gmp.R

--- CW_4/gmp.R
+++ CW_4/gmp.R
@@ -1,5 +1,6 @@
-#Загрузите данные в датафрейм. Адрес: github    https://raw???путь_к_файлу_найдите_сами???/data/gmp.dat 
-gmp <- ...
+#Загрузите данные в датафрейм.
+gmp <- read.table(file = "https://raw.githubusercontent.com/SergeyMirvoda/MD-DA-2017/master/data/gmp.dat")
+gmp$pop <- gmp$gmp / gmp$pcgmp
 
 estimate.scaling.exponent <- function(a, y0=6611, response=gmp$pcgmp,
                                         predictor = gmp$pop, maximum.iterations=100, deriv.step = 1/100,
@@ -18,7 +19,11 @@ estimate.scaling.exponent <- function(a, y0=6611, response=gmp$pcgmp,
 estimate.scaling.exponent(0.15)
 
 #С помошью полученного коэффициента постройте кривую (функция curve) зависимости
+y1 = 6611
+curve(y1 * x ^ estimate.scaling.exponent(0.15, y0 = y1)$a, xlab = "Население", ylab = "Доход на душу населения", from = 1, to = 1000);
 
 #Удалите точку из набора исходных данных случайным образом, как изменилось статистическая оценка коэффициента a?
 
+
 #Запустите оценку несколько раз с разных стартовых точек. Как изменилось значение a?
+
